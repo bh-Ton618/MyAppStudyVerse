270e52aba048b8ed5e55c3c3052d3eb498f990a4
bh-Ton618 <a_mb@tutamail.com>
2026-06-25 21:10:11 +0200

    Build first Compose Homescreen

--- app/src/main/java/com/example/myappstudyverse/MainActivity.kt
+++ app/src/main/java/com/example/myappstudyverse/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myappstudyverse.ui.theme.MyAppStudyVerseTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyAppStudyVerseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize(),       //in my composable there are other kid elements /composables
+    horizontalAlignment = Alignment.CenterHorizontally,
+     verticalArrangement = Arrangement.Center
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyAppStudyVerseTheme {
-        Greeting("Android")
+    {
+        Text(
+            text = "StudyVerse",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(
+            modifier = Modifier.height(24.dp)
+        )
+        Text(
+            text = "Willkommen zurück!"
+        )
+        Spacer(
+        modifier = Modifier.height(16.dp)
+        )
+        Button(
+            onClick = {
+            }
+        ) {
+            Text("Los geht's")
+        }
     }
 }
